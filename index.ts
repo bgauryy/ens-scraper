ef6312c97138935e287e3c6761e284423f60d9e3
GuyB <bgauryy@gmail.com>
2022-05-07 03:44:30 +0300

    update code

--- index.ts
+++ index.ts
@@ -2,19 +2,9 @@ import { run } from "./src/runner";
 
 process.setMaxListeners(0);
 
-const arr: string[] = [];
+const ethDomainsToCheck: string[] = [];
 for (let i = 30000; i < 100000; i++) {
-  arr.push(`${i}`);
+  ethDomainsToCheck.push(`${i}`);
 }
 
-//https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-shuffleArray(arr);
-
-run(arr);
+run(ethDomainsToCheck);
