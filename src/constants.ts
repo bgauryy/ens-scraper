2ac6adb0e1537c825dec56d625567d14b1efac59
GuyB <bgauryy@gmail.com>
2022-05-07 21:22:15 +0300

    rename files

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@
 export const BROWSER_POOL_SIZE = 5;
 export const PAGE_LIMIT = 8;
 export const CHUNK = BROWSER_POOL_SIZE * PAGE_LIMIT;
-export const UNAVAILABLE_FILE_NAME = "n-taken.txt";
 export const AVAILABLE_FILE_NAME = "n-available.txt";
 export const PREMIUM_FILE_NAME = "n-premium.txt";
+export const UNAVAILABLE_FILE_NAME = "n-unavailable.txt";
