182d1a2ddf2568426fe2fdbd6e5d37bc6bee40f4
GuyB <bgauryy@gmail.com>
2022-05-07 18:11:00 +0300

    change constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 //TODO - move to params
 //TODO - optimize dynamically
 export const BROWSER_POOL_SIZE = 5;
-export const PAGE_LIMIT = 5;
+export const PAGE_LIMIT = 8;
 export const CHUNK = BROWSER_POOL_SIZE * PAGE_LIMIT;
 export const TAKENFILE = "n-taken.txt";
 export const FREEDOMAINFILE = "n-available.txt";
